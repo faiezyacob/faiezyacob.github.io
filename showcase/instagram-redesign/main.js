996f891105eff147d684f451fbeaa96b223fff28
Nieda Yacob <53547654+faiezyacob@users.noreply.github.com>
2023-07-21 22:08:21 +0800

    Add double tap to like

--- showcase/instagram-redesign/main.js
+++ showcase/instagram-redesign/main.js
@@ -7,6 +7,7 @@ function onLoad() {
     
     function initClass() {
         likeBtns = document.querySelectorAll('.like-btn')
+        postImgs = document.querySelectorAll('.post-image')
 
         bindEvents();
     }
@@ -15,10 +16,19 @@ function onLoad() {
         likeBtns.forEach((el) => {
             el.addEventListener('click', onLikeBtnClick)
         })
+
+        postImgs.forEach((el) => {
+            el.addEventListener('dblclick', onPostImgDblClick);
+        })
     }
 
     function onLikeBtnClick() {
         this.querySelector('svg').classList.toggle('fill-red-400')
         this.querySelector('svg').classList.toggle('stroke-0')
+    }
+
+    function onPostImgDblClick() {
+        this.querySelector('div.like-btn svg').classList.toggle('fill-red-400');
+        this.querySelector('div.like-btn svg').classList.toggle('stroke-0');
     }
 }
