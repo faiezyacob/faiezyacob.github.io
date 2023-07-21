5e091f864fa5447cc7735b03f69b3c782d12b222
Nieda Yacob <53547654+faiezyacob@users.noreply.github.com>
2023-07-21 18:55:01 +0800

    Showcase initial commit

---
+++ showcase/instagram-redesign/main.js
@@ -0,0 +1,25 @@
+window.addEventListener('load', onLoad)
+
+function onLoad() {
+    let likeBtns = null
+
+    initClass()
+    
+    function initClass() {
+        likeBtns = document.querySelectorAll('.like-btn')
+
+        bindEvents();
+    }
+
+    function bindEvents() {
+        likeBtns.forEach((el) => {
+            el.addEventListener('click', onLikeBtnClick)
+        })
+    }
+
+    function onLikeBtnClick() {
+        console.log('ss');
+        this.querySelector('svg').classList.toggle('fill-red-400')
+        this.querySelector('svg').classList.toggle('stroke-0')
+    }
+}
