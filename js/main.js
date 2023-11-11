8e7f1b99b6f751524960e9cc0f1ac893e2d8a699
faiezyacob <53547654+faiezyacob@users.noreply.github.com>
2023-11-12 00:36:03 +0800

    Add particle effect.

--- js/main.js
+++ js/main.js
@@ -1,12 +1,14 @@
 window.addEventListener('load', onLoad);
 
 function onLoad() {
-	const ROTATION_SPEED = 0.025;
+	const PARTICLE_SPEED = 0.5;
+	const PARTICLE_NUMBER = 100;
 
 	initClass_();
 
 	function initClass_() {
 		initScrollSpy_();
+		initParticle_();
 		onScroll_();
 		bindEvents_();
 	}
@@ -14,6 +16,16 @@ function onLoad() {
 	function bindEvents_() {
 		document.addEventListener('scroll', onScroll_, { passive: true });
 		window.addEventListener('resize', onResize_);
+	}
+
+	function initParticle_() {
+		const options = {
+			container: '.particle__container',
+			speed: PARTICLE_SPEED,
+			number: PARTICLE_NUMBER,
+		}
+
+		new Particle_(options)
 	}
 	
 	function initScrollSpy_() {
@@ -29,4 +41,66 @@ function onLoad() {
 	function onResize_() {}
 
 	function onScroll_() {}
+
+	function Particle_(options) {
+		let containerEl_ = null;
+		let particlesArr = [];
+		let curPos = 0;
+
+		initClass__();
+
+		function initClass__() {
+			containerEl_ = document.querySelector(options.container);
+
+			setupStyle__();
+			generateParticles__();
+
+			animate__();
+		}
+
+		function animate__() {
+			for (let i = 0; i < particlesArr.length; i++) {
+				let curPos = particlesArr[i].top + options.speed;
+				if (curPos > containerEl_.offsetHeight) {
+					let posX = Math.floor(Math.random() * (containerEl_.offsetWidth - 1 + 1) + 1);
+					curPos = -5;
+					particlesArr[i].el.style.left = posX + 'px';
+
+				}
+				particlesArr[i].el.style.top =  curPos + 'px';
+				particlesArr[i].top = curPos;
+			}
+			requestAnimationFrame(animate__);
+		}
+
+		function generateParticles__() {
+			for (let i = 0; i < options.number; i++) {
+				let posX = Math.floor(Math.random() * (containerEl_.offsetWidth - 1 + 1) + 1);
+				let posY = Math.floor(Math.random() * (containerEl_.offsetHeight - 1 + 1) + 1);
+				let particle = document.createElement('div');
+				let particleObj = {};
+
+				particle.style.backgroundColor = '#fff';
+				particle.style.borderRadius = '50%';
+				particle.style.height = '10px';
+				particle.style.width = '10px';
+				particle.style.position = 'absolute';
+				particle.style.left = posX + 'px';
+				particle.style.top = posY + 'px';
+
+				particleObj.el = particle;
+				particleObj.top = posY;
+				particlesArr.push(particleObj);
+
+ 				containerEl_.appendChild(particle);
+			}
+		}
+
+		function setupStyle__() {
+			containerEl_.style.height = '100%';
+			containerEl_.style.overflow = 'hidden';
+			containerEl_.style.position = 'absolute';
+			containerEl_.style.width = '100%';
+		}
+	}
 }
