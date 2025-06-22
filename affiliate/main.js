cf6f2691672fe249265a60d64556325ff70ea3aa
faiezyacob <faiezyacob@gmail.com>
2025-06-22 14:49:34 +0800

    Remove affiliate folder

--- affiliate/main.js
+++
@@ -1,7 +0,0 @@
-window.addEventListener('load' , ()=> {
-    initParticle_();
-
-    function initParticle_() {
-		particlesJS.load('particles__container', 'particles.json', function () { });
-	}
-})
